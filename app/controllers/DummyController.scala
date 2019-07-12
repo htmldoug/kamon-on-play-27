6445f460bb251f7a6928bd5eae438cbd16691747
cspinetta <cebspinetta@gmail.com>
2019-07-12 01:24:21 -0300

    add logs

--- app/controllers/DummyController.scala
+++ app/controllers/DummyController.scala
@@ -14,41 +14,42 @@ class DummyController @Inject()(val controllerComponents: ControllerComponents,
                                 val someClient: SomeClient) extends BaseController with LogSupport {
 
   def get: Action[AnyContent] = Action {
+    logger.info(s"************************************")
+    logger.info(s"*** Handling ${controllers.routes.DummyController.get().url}")
+    logger.info(s"*** Tags: $currentTags")
+    logger.info(s"************************************")
     Ok(bodyResponse(controllers.routes.DummyController.get().url))
       .withHeaders(headers = "Custom-Header" -> "xxx")
   }
 
   def outgoingRequest: Action[AnyContent] = Action.async {
+    logger.info(s"************************************")
+    logger.info(s"*** Handling ${controllers.routes.DummyController.outgoingRequest().url}")
+    logger.info(s"*** Tags: $currentTags")
+    logger.info(s"*** Sending outgoing request to ${someClient.url}")
+    logger.info(s"************************************")
     someClient
       .call
       .map(response => {
         logger.info(s"Client response: $response")
-        Ok(bodyResponse(
-          name = controllers.routes.DummyController.outgoingRequest().url,
-          msg = Some(
-            s"""
-               |Outgoing call to ${someClient.url}
-               |Response:
-               |\tHeaders:
-               |\t\t${response.headers.mkString("[\n\t\t\t", "\n\t\t\t", "\n\t\t]")}
-               |\tStatus: ${response.statusText}
-               |\tBody: ${response.body}
-             """.stripMargin)))
+        Ok(bodyResponse(name = controllers.routes.DummyController.outgoingRequest().url))
       })
   }
 
+  private def currentTags: String = Kamon.currentContext()
+    .tags
+    .iterator()
+    .map(t => {
+      (t.key, Tag.unwrapValue(t).toString)
+    })
+    .mkString("[", ", ", "]")
+
   private def bodyResponse(name: String, msg: Option[String] = None): String = {
-    val tags = Kamon.currentContext()
-      .tags.iterator()
-      .map(t => {
-        (t.key, Tag.unwrapValue(t).toString)
-      })
-      .mkString("[", ", ", "]")
     s"""
        |Request to: $name
        |Trace ID: ${Kamon.currentSpan().trace.id.string}
        |Span ID: ${Kamon.currentSpan().id.string}
-       |Tags: $tags
+       |Tags: $currentTags
        |${msg.getOrElse("")}
        |""".stripMargin
   }
